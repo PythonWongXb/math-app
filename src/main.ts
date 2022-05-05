84bb97600bb29e43fd0e6cd80e88eed36358a356
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-05-05 21:15:55 +0800

    feat: styles

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: Evan Zuo v_wangxiangbo01@baidu.com
  * @Date: 2022-05-03 13:42:17
  * @LastEditors: Evan Zuo v_wangxiangbo01@baidu.com
- * @LastEditTime: 2022-05-03 20:45:49
+ * @LastEditTime: 2022-05-05 21:13:30
  * @FilePath: /vue-nestjs-ci-cd/micro-service/math/ms-app/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3025);
+  await app.listen(9000);
 }
 bootstrap();
